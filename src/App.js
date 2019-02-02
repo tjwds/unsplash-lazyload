cd4c689249660da40197f2094cb76a3c0ca4eda5
Joe Woods <woodsja@vcu.edu>
2019-02-02 16:29:36 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Unsplash from 'unsplash-js';
+import UNSPLASH_API_KEYS from './keys.js';
+
+const unsplash = new Unsplash({
+  applicationId: UNSPLASH_API_KEYS.KEY,
+  secret: UNSPLASH_API_KEYS.SECRET,
+})
+
+const blurry_query_string = "&fit=crop&w=100&q=10&fm=jpg";
+const normal_query_string = "&fit=crop&w=800&q=80&fm=jpg";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { images: [] };
+  }
+  
+  componentDidMount() {
+    this.getTargetPhotosAndLazyload();
+  }
+
+  getTargetPhotosAndLazyload = () => {
+    unsplash.photos.listPhotos(1, 15, "latest")
+    .then(response => response.json())
+    .then(body => {
+      this.renderLazyloading(body);
+    })
+  }
+
+  renderLazyloading = body => {
+    var results = [];
+    results = body.map(image => 
+      <div 
+        className="imageWrapper"
+        style={{ 
+          "backgroundColor": image.color,
+          "backgroundImage": 'url(' + image.urls.raw + blurry_query_string  + ')', 
+          "height": (800 / image.width) * image.height
+        }}
+      >
+        <img 
+          className="image image-fullsize" 
+          src={image.urls.raw + normal_query_string} 
+          alt={image.description} 
+        />
+      </div>
+    )
+
+    this.setState({images: results});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.images}
       </div>
     );
   }
---
+++ src/keys.example.js
@@ -0,0 +1,6 @@
+var UNSPLASH_API_KEYS = {
+  "KEY": "your api key",
+  "SECRET": "your secret key"
+}
+
+export default UNSPLASH_API_KEYS;
